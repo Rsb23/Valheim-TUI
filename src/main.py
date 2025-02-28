9e2b636bb41a859343f8f46c0d89f9bf9d495ff3
Rsb23 <rsbarbu23@gmail.com>
2025-02-27 20:48:56 -0500

    created external main.tcss, set default theme to 'tokyo-night', added title to TUI, added Collapsible elements to TUI, started code for automatically updating player count/list

--- src/main.py
+++ src/main.py
@@ -2,23 +2,30 @@ from datetime import datetime
 
 from data import InterpretData
 
+from art import text2art
+
 from textual.app import App, ComposeResult
-from textual.widgets import Collapsible, Label, Header, Footer
+from textual.containers import Horizontal
+from textual.widgets import Collapsible, Label, Header, Footer, Static
 
 
 class StatusApp(App):
-    CSS = """
-    Screen { align: center middle; }
-    Digits { width: auto; }
-    """
+    CSS_PATH = "main.tcss"
+
     def __init__(self, driver_class = None, css_path = None, watch_css = False, ansi_color = False):
-        super().__init__(driver_class, css_path, watch_css, ansi_color)
+        super().__init__(driver_class, css_path, watch_css, ansi_color)    
 
         self._InterpretData = InterpretData("./samplelog.txt")
+
         self.server_start_data = self._InterpretData.get_start_data()
+        self.player_count = self._InterpretData.get_connection_count()
+
+        self.theme = "tokyo-night"
 
     def compose(self) -> ComposeResult:  
         yield Header()
+        yield Static(text2art("Valheim"), id="title_1_label")
+        yield Static(text2art("Server Dashboard"), id="title_2_label")    
         with Collapsible(title="General"):
             with Collapsible(title="Server Details"):
                 yield Label(f"Loaded World: {self.server_start_data[0]}")
@@ -29,9 +36,25 @@ class StatusApp(App):
             with Collapsible(title="Joining"):
                 yield Label(f"Join Code: {self.server_start_data[5]}")
                 yield Label(f"External IP/PORT: {self.server_start_data[6]}")
-        with Collapsible(title=f"Connected Players: {self.server_start_data[7]}"):  # TODO: update this automatically with get_connection_count()       
+
+        with Collapsible(title=f"Connected Players: {self.player_count}", id="players_collapsible", disabled=True):  # TODO: update this automatically with get_connection_count()       
             pass  # TODO: call get_players() and add collapsible for each with labels for name, playfab id, and steam id
-        yield Footer()
+
+        # https://stackoverflow.com/questions/78814860/adding-status-text-to-a-textual-footer
+        with Horizontal(id="footer_outer"):
+            with Horizontal(id="footer_inner"):
+                yield Footer()
+            yield Label(f"Last Updated: {datetime.now().time()}", id="last_updated_label")  # TODO: update automatically
+        
+    def on_mount(self) -> None:
+        self.player_count = 0  # TESTING
+
+        if self.player_count > 0:
+            self.get_widget_by_id("players_collapsible").disabled = False
+        elif self.player_count == 0:
+            self.get_widget_by_id("players_collapsible").disabled = True
+        else:
+            raise ValueError("Can't have negative players!")
 
     
 if __name__ == "__main__":
